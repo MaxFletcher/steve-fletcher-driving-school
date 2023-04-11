06cf7aeee5ca1a39452a5ecc8e7719a9f4d0ade7
maxfletcher <maxfletcher01@hotmail.com>
2023-04-11 23:42:55 +0100

    Creates initial website files and folder structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+//import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
+import App from './App.vue'
+import routes from '~pages';
+
+import './assets/main.css'
+
+export const createApp = ViteSSG(
+    // the root component
+    App,
+    // vue-router options
+    { routes },
+    // function to have custom setups
+    ({ app, router, routes, isClient, initialState }) => {
+        // install plugins etc.
+    },
+)
