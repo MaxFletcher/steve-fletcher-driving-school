8b10a7c498f253c7c730f57193e3a726da132602
maxfletcher <maxfletcher01@hotmail.com>
2023-04-16 23:30:41 +0100

    Adds type="module" to package.json

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import routes from '~pages';
 import './assets/main.css'
 
 export const createApp = ViteSSG(
-    // the root component
-    App,
-    // vue-router options
-    { routes },
-    // function to have custom setups
-    ({ app, router, routes, isClient, initialState }) => {
-        // install plugins etc.
-    },
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  },
 )
